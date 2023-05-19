ef0dc659dffe10029fe4f55c210f42277ac89e78
ruanxiangyu <ruanxiangyu@pcoin.com>
2023-05-19 09:35:07 +0000

    a web server

--- test.go
+++ test.go
@@ -1,8 +1,54 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+)
 
 func main() {
-   fmt.Println("Hello, World!")
+
+	mux := http.NewServeMux()
+
+	mux.Handle("/", &helloHandler{})
+
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+
+	// 创建系统信号接收器
+
+	done := make(chan os.Signal)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		<-done
+		if err := server.Shutdown(context.Background()); err != nil {
+			log.Fatal("Shutdown server:", err)
+		}
+
+	}()
+
+	log.Println("Starting HTTP server...")
+	err := server.ListenAndServe()
+
+	if err != nil {
+
+		if err == http.ErrServerClosed {
+			log.Print("Server closed under request")
+		} else {
+			log.Fatal("Server closed unexpected")
+		}
+	}
 }
-// sdf
+
+type helloHandler struct{}
+
+func (*helloHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World")
+}
